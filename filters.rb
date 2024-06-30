35d8e2f0c8cb751449593af4b84f0a05cc528e1e
ChrisPytel <chris_pytel@hotmail.com>
2024-06-29 22:18:34 -0400

    implement logic for the find(id) method to filter candidates with matching ID's

--- filters.rb
+++ filters.rb
@@ -1,9 +1,24 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
+# find(id) Returns the candidate with that :id
+# If there is no candidate with that id, it naturally returns nil
 def find(id)
-    # Your code Here
+  puts "ID number to check is: #{id}"
+
+  for candidate in @candidates
+    # puts "This candidate is: "
+    # pp  candidate
+    puts "ID for this candidate is: #{candidate[:id]}"
+    if candidate[:id] == id
+      puts "gotta match"
+      return candidate      
+    end 
   end
+  return nil; # return nil is implicit, but added here as a reminder
+end 
+# ------------------------------------------------------------------------------------------------
+  
   
   def experienced?(candidate)
     # Your code Here
